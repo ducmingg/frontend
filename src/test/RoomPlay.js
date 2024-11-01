9e037f4e567a8ce5fe77fe8b8beba93dd2d7825d
BichPhuong <21130488@st.hcmuaf.edu.vn>
2024-11-01 22:20:04 +0700

    test create private room no password

--- src/test/RoomPlay.js
+++ src/test/RoomPlay.js
@@ -12,6 +12,8 @@ export async function runCreateRoomTest() {
         await driver.sleep(2000);
         await testEmptyFields(driver);
         await driver.sleep(2000);
+        await testCreatePrivateRoomNoPassword(driver);
+        await driver.sleep(2000);  // Optional: Wait for the page to load
 
     } catch (e) {
         console.error("Test execution failed - " + e.message);
@@ -39,6 +41,22 @@ async function testEmptyFields(driver) {
         console.log("Test empty fields: Passed");
     } catch (e) {
         console.log("Test empty fields: Failed - " + e.message);
+    }
+}
+
+async function testCreatePrivateRoomNoPassword(driver) {
+    await resetCreateRoomForm(driver);
+    try {
+        await driver.findElement(By.xpath("/html/body/div[2]/div[3]/div/div[2]/div[1]/div/input")).sendKeys("Private Room");
+        await driver.findElement(By.xpath("/html/body/div[2]/div[3]/div/div[2]/div[3]/label[2]/span[1]/span[1]/input")).click();
+        let createButton = await driver.findElement(By.xpath("/html/body/div[2]/div[3]/div/div[3]/button[2]/span[2]"));
+        await driver.executeScript("arguments[0].click();", createButton);
+        await driver.sleep(2000);
+        let error = await driver.findElement(By.xpath('/html/body/div[2]/div[3]/div/div[2]/div[5]/div/div')).getText();
+        assert.strictEqual(error, 'Password cannot leave empty in Private mode!');
+        console.log("Test create private room no password: Passed");
+    } catch (e) {
+        console.log("Test create private room no password: Failed - " + e.message);
     }
 }
 
